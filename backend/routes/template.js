649bdf761320b723d30825fe484ea1a1433d50e4
Sparsh-tyagi01 <sparshtyagi630@gmail.com>
2025-12-15 00:10:23 +0530

    create multiupload option

--- backend/routes/template.js
+++ backend/routes/template.js
@@ -3,10 +3,15 @@ const router = express.Router()
 const {createTemplate, getTemplates, getTemplatesById, deleteTemplate, updateTemplate} = require("../controllers/template")
 const {verifyToken, adminOnly} = require("../middlewares/auth")
 
-router.post("/create", verifyToken, adminOnly ,createTemplate)
+const multiUpload = upload.fields([
+    { name: "image", maxCount: 1 },
+    { name: "html", maxCount: 1 }
+]);
+
+router.post("/create", verifyToken, adminOnly, multiUpload, createTemplate)
 router.get("/all", getTemplates)
 router.get("/:id", verifyToken, getTemplatesById)
 router.delete("/:id", verifyToken, adminOnly, deleteTemplate)
-router.put("/:id", verifyToken, adminOnly, updateTemplate)
+router.put("/:id", verifyToken, adminOnly, multiUpload, updateTemplate)
 
 module.exports = router
